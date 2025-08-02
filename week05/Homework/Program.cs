9d3caa10c72156647c2aaa048294797f41f76872
Afcyare <fabdim7@gmail.com>
2025-08-02 19:03:03 +0300

    Added W05 Learning Activity: Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
-    }
+        Assignment assignment1 = new Assignment("samuel Bennet", "Multiplication");
+        // Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+
+        // System.Console.WriteLine(mathAssignment1.GetSummary());
+        // System.Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        System.Console.WriteLine(writingAssignment.GetSummary());
+        System.Console.WriteLine(writingAssignment.GetWritingInformation());
+    }        
 }
